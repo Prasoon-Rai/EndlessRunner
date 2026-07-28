0efc46ad6428db4063e138bb56bfeff7815c20dc
Prasoon Rai <prasoonrai181@gmail.com>
2026-07-28 20:08:35 +0530

    Added essential files and made a small visual environment.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+mod load_models;
+use load_models::load_models;
+use raylib::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    const WIDTH: i32 = 1000;
+    const HEIGHT: i32 = 600;
+    let (mut rl, thread): (RaylibHandle, RaylibThread) = raylib::init()
+        .size(WIDTH, HEIGHT)
+        .title("Endless Runner")
+        .build();
+
+    let game_models = load_models(&mut rl, &thread);
+
+    let mut camera = Camera3D {
+        position: Vector3::new(5.0, 5.0, 5.0),
+        target:   Vector3::new(0.0, 0.0, 0.0),
+        up:       Vector3::new(0.0, 1.0, 0.0),
+        fovy:     45.0,
+        projection: CameraProjection::CAMERA_PERSPECTIVE,
+    };
+
+    rl.disable_cursor();
+
+    while !rl.window_should_close() {
+        let mut d: RaylibDrawHandle = rl.begin_drawing(&thread);
+        d.clear_background(Color::BLACK);
+        {
+            let mut d3: RaylibMode3D<RaylibDrawHandle> = d.begin_mode3D(camera);
+            d3.draw_grid(20, 1.0);
+        }
+    }
 }
